992b96c4743b318095246c117bb828e84e1c3147
David Parrish <daveparrish@tutanota.com>
2019-03-12 21:02:33 -0400

    Separate tor service from bitcoin and lnd service containers.
    Comment out bitcoin and lnd for now.

---
+++ docker/tor/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# Create torrc if it doesn't exist
+if [ ! -f "/etc/tor/torrc" ]; then
+    cp /tmp/torrc /etc/tor/torrc
+fi
+
+# Change local user id and group
+if [ -n "${LOCAL_USER_ID}" ]; then
+    usermod -u "$LOCAL_USER_ID" alice
+    groupmod -g "$LOCAL_USER_ID" alice
+fi
+
+# Set correct owners on volumes
+chown -R tor:alice "${TOR_DATA}"
+chown -R :alice /etc/tor
+chown -R alice:alice /home/alice
+
+exec sudo -u tor /usr/bin/tor

35949f9e91dd7de560f7803ae7a0c4ba19b94928
David Parrish <daveparrish@tutanota.com>
2019-11-29 09:44:52 -0500

    Add electrs and settings to allow for testnet and mainnet

---
+++ docker/electrs/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+# Create config file if it doesn't already exist
+if [ ! -f "/home/user/.electrs/config.toml" ]; then
+    envsubst < /tmp/electrs.toml > /home/user/.electrs/config.toml
+fi
+
+# Change local user id and group
+if [ -n "${LOCAL_USER_ID}" ]; then
+    usermod -u "$LOCAL_USER_ID" user
+fi
+if [ -n "${LOCAL_GROUP_ID}" ]; then
+    groupmod -g "$LOCAL_GROUP_ID" user
+fi
+
+# Fix ownership
+chown -R user /home/user/.electrs
+
+pwd
+ls -alh /home/user
+
+# Start electrs
+exec sudo -u user "$@"

db7932f0858fc8681c97f5a6a71ca412b38de214
David Parrish <daveparrish@tutanota.com>
2019-11-29 13:36:52 -0500

    Add macaroons script for printing hex of macaroon for Zeus LN app

---
+++ macaroons.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Print macaroons in hex format for Zeus app
+
+network="mainnet"
+macaroon="admin"
+
+if [ -n "$1" ]; then
+    network="$1"
+fi
+
+if [ -n "$2" ]; then
+    macaroon_name="$2"
+fi
+
+macaroon_hex="$(docker-compose exec -u lnd lnd sh -c "cat /home/lnd/.lnd/data/chain/bitcoin/\"$network\"/\"$macaroon_name\".macaroon" | xxd -p | tr -d '\n')"
+
+echo "$network $macaroon macaroon (in hex):"
+echo "$macaroon_hex"

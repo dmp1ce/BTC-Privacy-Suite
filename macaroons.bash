98004b4cb9fc8659984bd13be19938f8b16d8b9b
David Parrish <daveparrish@tutanota.com>
2019-12-15 16:52:08 -0500

    Fix macaroons.bash variable name

--- macaroons.bash
+++ macaroons.bash
@@ -3,7 +3,7 @@
 # Print macaroons in hex format for Zeus app
 
 network="mainnet"
-macaroon="admin"
+macaroon_name="admin"
 
 if [ -n "$1" ]; then
     network="$1"
@@ -15,5 +15,5 @@ fi
 
 macaroon_hex="$(docker-compose exec -u lnd lnd sh -c "cat /home/lnd/.lnd/data/chain/bitcoin/\"$network\"/\"$macaroon_name\".macaroon" | xxd -p | tr -d '\n')"
 
-echo "$network $macaroon macaroon (in hex):"
+echo "$network $macaroon_name macaroon (in hex):"
 echo "$macaroon_hex"

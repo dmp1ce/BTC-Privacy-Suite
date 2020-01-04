62f17e97a01d8de54361654bb90c85f7f8451fca
David Parrish <daveparrish@tutanota.com>
2020-01-04 12:02:23 -0500

    Allow start.bash and build.bash to work with yml overrides

--- build.bash
+++ build.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-docker-compose build --pull --build-arg LOCAL_USER_ID="$(id -u "$USER")" --build-arg LOCAL_GROUP_ID="$(id -g "$USER")"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Get list of overrides
+OVERRIDE_OPTIONS=()
+shopt -s nullglob
+for f in "$DIR/overrides"/*.yml
+do
+    OVERRIDE_OPTIONS+=(-f)
+    OVERRIDE_OPTIONS+=("$f")
+done
+
+docker-compose -f docker-compose.yml "${OVERRIDE_OPTIONS[@]}" build --pull \
+               --build-arg LOCAL_USER_ID="$(id -u "$USER")" \
+               --build-arg LOCAL_GROUP_ID="$(id -g "$USER")"
--- start.bash
+++ start.bash
@@ -2,6 +2,23 @@
 
 # Start LND, Bitcoin and Tor with added overrides
 
+CMD=""
+case "$1" in
+    "help" )
+        echo "Start services with overrides. Supported parameters are 'up' and 'restart'."
+        echo "Example: \`./start restart bitcoin\`"
+        echo ""
+        echo "Default command is \`up -d\`"
+        exit 0
+        ;;
+    "restart" | "up" | "logs" | "ps" | "stop" | "exec" | "kill" )
+        read -r -a CMD <<< "$@"
+        ;;
+    *)
+        CMD=(up -d)
+        ;;
+esac
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Get list of overrides
@@ -14,5 +31,5 @@ do
 done
 
 # Start docker-compose with override options
-echo docker-compose -f docker-compose.yml "${OVERRIDE_OPTIONS[@]}" up -d
-cd "$DIR" && exec docker-compose -f docker-compose.yml "${OVERRIDE_OPTIONS[@]}" up -d
+echo docker-compose -f docker-compose.yml "${OVERRIDE_OPTIONS[@]}" "${CMD[@]}"
+cd "$DIR" && exec docker-compose -f docker-compose.yml "${OVERRIDE_OPTIONS[@]}" "${CMD[@]}"

3c5efe68af93a6959159022ebb1f19b62e7d992c
David Parrish <daveparrish@tutanota.com>
2020-02-19 18:52:36 -0500

    Add bitcoin server to onions.bash

--- onions.bash
+++ onions.bash
@@ -3,5 +3,7 @@
 # Print onion addresses
 
 electrs_onion="$(docker-compose exec -u tor tor cat /var/lib/tor/electrs/hostname)"
+bitcoin_onion="$(docker-compose exec -u bitcoin bitcoin bitcoin-cli getnetworkinfo | python3 -c "import sys, json; print(json.load(sys.stdin)['localaddresses'][0]['address'])")"
 
+echo "Bitcoin Server: $bitcoin_onion"
 echo "Electrum Server: $electrs_onion"

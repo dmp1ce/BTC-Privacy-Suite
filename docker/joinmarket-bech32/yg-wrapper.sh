7b77f974f960adbaf378836176fecc60c77ccb00
David Parrish <daveparrish@tutanota.com>
2021-01-21 19:22:10 -0500

    Removed old bech32 specific Dockerfile configuration

--- docker/joinmarket-bech32/yg-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-WALLET=${JM_WALLET_FILE:-"yg.jmdat"}
-PASS=${JM_WALLET_PASSWORD:-"password"}
-SCRIPT=${JM_YG_SCRIPT:-"yield-generator-basic.py"}
-
-exec printf "%s" "$PASS" | python3 "$SCRIPT" "$WALLET" --wallet-password-stdin

9f5c74179f2082858feaca64818644fed79a78be
David Parrish <daveparrish@tutanota.com>
2021-06-16 16:52:37 -0400

    Only detect duplicate variables for duplicates in on yml file at a time

--- scripts/detect_duplicate_vars.bash
+++ scripts/detect_duplicate_vars.bash
@@ -7,43 +7,44 @@ DIR=${DIR:-.}
 # shellcheck source=continueYN.bash
 . "$DIR/scripts/continueYN.bash"
 
-if compgen -G "${DIR}/overrides/*.yml" > /dev/null; then
-    mapfile -t required_envs < <(grep -ho "env\/[a-zA-Z0-9_-]\+\.env" "$DIR"/overrides/*.yml "$DIR"/docker-compose.yml)
-else
-    mapfile -t required_envs < <(grep -ho "env\/[a-zA-Z0-9_-]\+\.env" "$DIR"/docker-compose.yml)
-fi
-required_envs+=(".env")
+# Loop through each yml file
+for yml in "$DIR"/overrides/*.yml "$DIR"/docker-compose.yml; do
 
-# For the second loop
-required_envs2=( "${required_envs[@]}" )
+    mapfile -t required_envs < <(grep -ho "env\/[a-zA-Z0-9_-]\+\.env" "$yml")
 
-# Loop through all env files looking for any duplicate variables in different files.
-for e in "${required_envs[@]}"; do
+    # Always add .env
+    required_envs+=(".env")
 
-    # Use realpath for greps to work
-    e_real=$(realpath "$e")
+    # For the second loop
+    required_envs2=( "${required_envs[@]}" )
 
-    # Remove env files already searched as not to give duplicate warnings
-    required_envs2=( "${required_envs2[@]/$e}" )
-
-    for e2 in "${required_envs2[@]}"; do
-        if [ "$e" = "$e2" ]; then continue; fi
-        if [ "$e2" = "" ]; then continue; fi
+    # Loop through all env files looking for any duplicate variables in different files.
+    for e in "${required_envs[@]}"; do
 
         # Use realpath for greps to work
-        e2_real=$(realpath "$e2")
+        e_real=$(realpath "$e")
 
-        dup_detected=0
-        while IFS= read -r k; do
-            while IFS= read -r k2; do
-                if [ "$k" = "$k2" ] ; then
-                    echo "WARNING: Detected duplicate variable '$k' in '$e' and '$e2' env files!"
-                    dup_detected=1
-                fi
-            done < <(grep -ho "^[a-zA-Z0-9_-]\+" < "$e2_real")
-        done < <(grep -ho "^[a-zA-Z0-9_-]\+" < "$e_real")
+        # Remove env files already searched as not to give duplicate warnings
+        required_envs2=( "${required_envs2[@]/$e}" )
 
-        if [ $dup_detected = 1 ]; then continueYN; fi
+        for e2 in "${required_envs2[@]}"; do
+            if [ "$e" = "$e2" ]; then continue; fi
+            if [ "$e2" = "" ]; then continue; fi
+
+            # Use realpath for greps to work
+            e2_real=$(realpath "$e2")
+
+            dup_detected=0
+            while IFS= read -r k; do
+                while IFS= read -r k2; do
+                    if [ "$k" = "$k2" ] ; then
+                        echo "WARNING: Detected duplicate variable '$k' in '$e' and '$e2' env files reference by '$yml'!"
+                        dup_detected=1
+                    fi
+                done < <(grep -ho "^[a-zA-Z0-9_-]\+" < "$e2_real")
+            done < <(grep -ho "^[a-zA-Z0-9_-]\+" < "$e_real")
+
+            if [ $dup_detected = 1 ]; then continueYN; fi
+        done
     done
 done
-

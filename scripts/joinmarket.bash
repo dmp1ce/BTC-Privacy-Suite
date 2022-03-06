f20a39a1ff875470ec1e507f32e49cfe1cb89241
David Parrish <daveparrish@tutanota.com>
2022-03-06 07:41:21 -0500

    Automatically load or create bitcoind wallet for JoinMarket wallet functions

--- scripts/joinmarket.bash
+++ scripts/joinmarket.bash
@@ -30,6 +30,31 @@ if ! ./start ps -q "$DAEMON_SERVICE" > /dev/null 2>&1; then
     exit 1
 fi
 
+BITCOIN_SERVICE=${BITCOIN_SERVICE:-bitcoin}
+BITCOIN_WALLET_NAME=${BITCOIN_WALLET_NAME:-jm}
+
+# Is a Bitcoin wallet loaded?
+if ! ./start ps -q "$BITCOIN_SERVICE" > /dev/null 2>&1; then
+    echo "Bitcoin service ($BITCOIN_SERVICE) is not currently running."
+    echo "Was the Bitcoin override enabled?"
+    echo "See: https://github.com/dmp1ce/BTC-Privacy-Suite##enabling-overrides"
+    exit 1
+fi
+
+# Look for a loaded wallet
+if ! ./start exec -T -u bitcoin bitcoin bitcoin-cli listwallets | python3 -c "import sys, json; print(json.load(sys.stdin)[0])" > /dev/null 2>&1; then
+
+    # No wallet loaded so try to load a hard coded wallet
+    if ! ./start exec -T -u bitcoin bitcoin bitcoin-cli loadwallet "$BITCOIN_WALLET_NAME" true > /dev/null 2>&1; then
+
+        # Couldn't load the hard coded wallet so try to create it.
+        if ! ./start exec -T -u bitcoin bitcoin bitcoin-cli -named createwallet wallet_name="$BITCOIN_WALLET_NAME" load_on_startup=true > /dev/null 2>&1; then
+            echo "No bitcoin wallet available!"
+            echo "Failed to load and create wallet '$BITCOIN_WALLET_NAME'."
+            echo "Please make sure a wallet is loaded in bitcoind for JoinMarket wallet functions."
+            exit 1
+fi; fi; fi
+
 CMD=()
 WALLET="${WALLET:-wallet.jmdat}"
 case "$1" in

b436024ea28cd240970185265ad9485e130f0201
David Parrish <daveparrish@tutanota.com>
2022-04-12 18:48:06 -0400

    Remove --dry-run for charge-lnd

--- docker/charge-lnd/charge-lnd-wrapper.bash
+++ docker/charge-lnd/charge-lnd-wrapper.bash
@@ -8,6 +8,4 @@ if [ "$NETWORK" = "testnet" ]; then
     _OPTIONS=("${_OPTIONS[@]}" "--macaroon" "$HOME/.lnd/data/chain/bitcoin/testnet/admin.macaroon")
 fi
 
-echo "Running charge-lnd with --dry-run"
-echo "OPTIONS: ${_OPTIONS[*]}"
-exec charge-lnd "${_OPTIONS[@]}" --dry-run
+exec charge-lnd "${_OPTIONS[@]}"
